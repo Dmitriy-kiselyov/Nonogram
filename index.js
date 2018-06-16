ee502c03e2fdcd91260b754923a194a7dfb4d16f
dmitriy96 <dmitriy96@yandex-team.ru>
2018-06-16 23:41:58 +0300

    State

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ const extractor = require('./lib/extractor');
 const url = 'http://www.nonograms.ru/nonograms2/i/18930';
 extractor(url)
     .then((nonogram) => {
-        console.log('Width:', nonogram.width);
-        console.log('Height:', nonogram.height);
-        console.log('Colors:', nonogram.colors);
-        console.log('Top:', nonogram.top);
-        console.log('Left:', nonogram.left);
+        console.log(nonogram.toString());
     });
