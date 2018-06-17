971fb8b1859139bafc703a8920e5c58274622b16
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-06-17 20:25:28 +0300

    Solver

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const extractor = require('./lib/extractor');
+const Solver = require('./lib/solver');
 
 const url = 'http://www.nonograms.ru/nonograms2/i/18930';
 extractor(url)
     .then((nonogram) => {
+        const solver = new Solver(nonogram);
+        solver.solve();
+
         console.log(nonogram.toString());
     });
